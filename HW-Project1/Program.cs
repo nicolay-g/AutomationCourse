a711882d04c84b3aafd7845fa770b97e6c737166
nicolay-g <ng82@mail.bg>
2022-06-04 08:08:07 +0300

    Add project files.

---
+++ HW-Project1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
