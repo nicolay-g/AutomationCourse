5a7d1178e2dd8135f4ab89414a670a8d443968b3
nicolay-g <ng82@mail.bg>
2022-06-04 08:30:02 +0300

    Initial School implementation

---
+++ HW-Project1/School.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_8
+{
+    class School
+    {
+        public School(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            ListOfStudents = new List<Student>();
+        }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public List<Student> ListOfStudents { get; set; }
+
+        public void AddStudent(Student student)
+        {
+            ListOfStudents.Add(student);
+        }
+
+        public void RemoveStudent(int id)
+        {
+            if ((id >= 1) && (id < ListOfStudents.Count))
+            {
+                ListOfStudents.RemoveAt(id);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Invalid Student id.");
+            }
+        }
+
+        public List<Student> GetExcellentStudents(List<Student> listOfStudents)
+        {
+            List<Student> excellentStudents = new List<Student>();
+
+            foreach (var student in listOfStudents)
+            {
+                if (student.IsExcellent)
+                {
+                    excellentStudents.Add(student);
+                }
+            }
+            return excellentStudents;
+        }
+    }
+}
---
+++ HW-Project1/Student.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_8
+{
+    class Student
+    {
+        private const int AdultAge = 18;
+
+        private int _age;
+        
+        public Student(int id, string name, int age, Dictionary<string, int> subjectsMarks)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.Age = age;
+            this.SubjectsMarks = subjectsMarks;
+        }
+
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value >= 7)
+                {
+                    this._age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Student is less than 7 years old!");
+                }
+            }
+        }
+
+        public Dictionary<string, int> SubjectsMarks { get; set; }
+
+        public bool IsExcellent 
+        {
+            get
+            {
+                return IsExcellentStudent();
+            } 
+        }
+
+        private bool IsExcellentStudent()
+        {
+            bool isExcellent = true;
+
+            foreach (var subject in SubjectsMarks)
+            {
+                if (subject.Value != 6)
+                {
+                    isExcellent = false;
+                    break;
+                }
+            }
+
+            return isExcellent;
+        }
+
+        public void Speak()
+        {
+            if (this.Age == AdultAge)
+            {
+                Console.WriteLine("Hello I'm {0} and I'll graduate this year", Name);
+            }
+            else
+            {
+                Console.WriteLine("Hello I'm {0} and I've got {1} years to graduate.", Name, AdultAge - Age);
+            }
+        }
+    }
+}
